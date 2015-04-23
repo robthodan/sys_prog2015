8b1d3acf40eefe493fc679a10b67f0d874f2f674
Tom <thomas.buck@me.com>
2015-04-23 18:42:25 +0200

    -

--- Automat/src/NumberState.cpp
+++ Automat/src/NumberState.cpp
@@ -15,7 +15,7 @@ void NumberState::read(char c, AutomatOO* automat){
 	if(!((48 <= c) && (c <= 57))){
 		std::cout << "Number" << std::endl;
 		automat->setStateInitial();
-		automat->counter--;
+
 	}
 
 
